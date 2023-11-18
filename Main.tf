f93fa67b72ff6fe344b16203c2bccca07405fb22
shiva prasad Badugu <shiva.badugu27@gmail.com>
2023-11-18 14:37:44 +0530

    built 3 machines using terraform

--- Main.tf
+++ Main.tf
@@ -12,15 +12,21 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-0a6ed6689998f32a5"
+    ami = "ami-0287a05f0ef0e9d9a"
     instance_type = "t2.micro"
     key_name = "Mykeypair"
-    security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.demo-public-subnet-01.id
+    for_each = toset([ "jenkins-master","build-slave","ansible" ])
+    tags = {
+      Name = "${each.key}"
+    }
 }
 
 resource "aws_security_group" "demo-sg" {
     name = "demo-sg"
     description = "SSH Access"
+    vpc_id = aws_vpc.demo-vpc.id
 
     ingress {
     description      = "SSH Access"
@@ -43,3 +49,57 @@ resource "aws_security_group" "demo-sg" {
   }
   
 }
+
+resource "aws_vpc" "demo-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+        name = "demo-vpc"
+    }
+}
+
+resource "aws_subnet" "demo-public-subnet-01" {
+    vpc_id = aws_vpc.demo-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "ap-south-1a"
+    tags = {
+      "name" = "demo-public-subnet-01" 
+    }
+}
+
+resource "aws_subnet" "demo-public-subnet-02" {
+    vpc_id = aws_vpc.demo-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "ap-south-1b"
+    tags = {
+      "name" = "demo-public-subnet-02" 
+    }
+}
+
+resource "aws_internet_gateway" "demo-IGW" {
+    vpc_id = aws_vpc.demo-vpc.id
+    tags = {
+      Name = "demo-IGW"
+    }
+  
+}
+
+resource "aws_route_table" "demo-public-rt" {
+  vpc_id = aws_vpc.demo-vpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demo-IGW.id
+  }
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-01" {
+    subnet_id = aws_subnet.demo-public-subnet-01.id
+    route_table_id = aws_route_table.demo-public-rt.id
+}
+
+resource "aws_route_table_association" "demo-rta-public-subnet-02" {
+    subnet_id = aws_subnet.demo-public-subnet-02.id
+    route_table_id = aws_route_table.demo-public-rt.id
+}
+
